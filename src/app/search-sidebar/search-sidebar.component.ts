3302372ea13321f9aa58809b27d3c385462c020a
Rachit Solanki <rachit.solanki@infostretch.com>
2017-11-24 14:54:44 +0530

    searchbar enhancement

--- src/app/search-sidebar/search-sidebar.component.ts
+++ src/app/search-sidebar/search-sidebar.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchSidebarComponent implements OnInit {
 
+  private searchForm:object={
+  	origin:'',
+  	destination:'',
+  	departure_date:'',
+  	return_date:'',
+  	no_of_passengers:0
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  search(){
+  	console.log(this.searchForm);
+  }
+
 
 }
