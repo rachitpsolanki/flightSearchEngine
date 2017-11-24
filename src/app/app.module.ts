d0500672cc956b206fd40014a06ef12449d41200
Rachit Solanki <rachit.solanki@infostretch.com>
2017-11-24 13:55:38 +0530

    flight search wireframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBodyComponent } from './search-body/search-body.component';
+import { SearchSidebarComponent } from './search-sidebar/search-sidebar.component';
+import { SearchItemComponent } from './search-item/search-item.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SearchBodyComponent,
+    SearchSidebarComponent,
+    SearchItemComponent,
+    FilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
